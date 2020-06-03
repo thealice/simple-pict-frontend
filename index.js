735d06a8cbb04ded15a705c1a01260b157f69658
Alice Freda <alicefreda@gmail.com>
2020-06-03 12:13:48 -0700

    begin building getThemes function

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+const baseURL = "http://localhost:3000/api/v1/";
+const promptsArr = [];
+const themesArr = [];
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("hi, i've loaded")
+    getThemes();
+    // ask what theme and team names
+    // then fetch prompts from that theme
+    // should i getThemes() and getPrompts() separately and store them
+    // load instructions and team names form
+    // show team name whose turn it is and begin gameplay button
+    // begin gameplay
 });
+
+function getThemes() {
+
+    fetch(`${baseURL}themes`)
+    .then(res => res.json())
+    .then(themes => {
+        themes.data.forEach(theme => {
+            themesArr.push(theme.attributes.name)
+        })
+    })
+    return themesArr;
+}
+
+// function getPrompts() {
+//     fetch(endPoint)
+//     .then(res => res.json())
+//     .then(prompts => {
+//         prompts.data.map(prompt => {
+//             promptsArr.push(prompt.attributes.content)
+//             // should i make separate arrays for each theme here?
+//         })
+//         console.log(promptsArr);
+//     })
+// }
+
+// JS Classes
