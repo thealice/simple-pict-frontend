83ec1770f8d9f1c4814c71a3c33bb087b5c0624d
Alice Freda <alicefreda@gmail.com>
2020-06-03 17:26:18 -0700

    begin building out game setup forms

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const baseURL = "http://localhost:3000/api/v1/";
 
 document.addEventListener('DOMContentLoaded', () => {
-    getThemes();
-
-    // load theme names into form for users to select a theme or no theme
-
-    // ask what theme and team names
-    // then fetch prompts from that theme
+    // renderCanvas();
+    let themes = getThemes();
+    // let themeNames = loadThemeNames(themes);
+    loadModal(); 
+    // see notes.md to figure out flow here
+    // one idea: Load instructions and setup form with: 
+        // enter team name
+        // select a theme or no theme
+        // submit button
+    // fetch prompts from that theme
     // or get all prompts if user selects "none"
         // getAllPrompts();
-    // should i getThemes() and getPrompts() separately and store them
-    // load instructions and team names form
+    // ask if player wants to add a prompt
     // show team name whose turn it is and begin gameplay button
     // begin gameplay
 });
@@ -26,6 +29,42 @@ function getThemes() {
         })
     })
     return(themesArr);
+}
+
+function loadThemeNames(themeObjects) {
+    return themeObjects.map(theme => {
+        return this.push(theme.name)
+    })
+    
+}
+
+function loadModal() {
+    const modal = document.getElementById("instructionsModal")
+    const p = document.createElement("p")
+
+    const teamForm = document.createElement("form")
+ 
+    p.innerText = `Hi, Welcome to Simple Pictionary!`
+
+    teamForm.setAttribute("id", "game-setup")
+    teamForm.innerHTML = `
+        <fieldset>
+            <legend>Setup your teams:</legend>
+            <div>
+            <label for="team1_name">
+              Team1 Name:
+            </label><br>
+                <input type="text" name="team1_name" id="team1_name" placeholder="Enter first Team name...">
+            </div>
+          <div>
+            <label for="team2_name">Team 2 Name:</label><br>
+            <input type="text" name="team2_name" id="team2_name" placeholder="Enter second Team name...">
+          </div>
+        </fieldset>
+        `
+    modal.appendChild(p);
+    modal.appendChild(teamForm);
+
 }
 
 
