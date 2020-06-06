317cc69f351e9a7950a25edb8a73a3e811148d89
Alice Freda <alicefreda@gmail.com>
2020-06-05 19:44:41 -0700

    game contructor

--- src/game.js
+++ src/game.js
@@ -1,9 +1,17 @@
 
 class Game {
     constructor(team1, team2, theme="all") {
-        this.team1 = team1;
-        this.team2 = team2;
-        this.theme = theme;
-        this.scoreCard = `${team1.name}: ${team1.score}, ${team2.name}: ${team2.score}`
+        this.team1 = new Team(team1);
+        this.team2 = new Team(team2);
+        if (theme === "all") {
+            this.theme = "All themes"
+            this.prompts = Prompt.all
+        } else {
+            this.theme = Theme.all.find(themeObj => themeObj.name === theme);
+            this.prompts = this.theme.prompts
+        }
+        // this.theme = Theme.all.find(theme);
+        this.scorecard = `${this.team1.name}: ${this.team1.score}, ${this.team2.name}: ${this.team2.score}`;
+        console.log(this);
     }
 }
