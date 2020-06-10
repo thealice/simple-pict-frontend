147c723b47d9f6bb506b05ecaaffcbebc9bf2e3b
Alice Freda <alicefreda@gmail.com>
2020-06-09 18:22:12 -0700

    get canvas ready for drawing

--- src/drawboard.js
+++ src/drawboard.js
@@ -1,8 +1,16 @@
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
+canvas.width = "1242px";
 canvas.height = window.innerHeight;
 canvas.style.border = "2px solid skyblue";
+ctx.strokeStyle = "black";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 100;
+let isDrawing = false;
+// where to start and stop the line
+let lastX = 0;
+let lastY = 0; 
 
 function loadDrawboard () {
     // hide instructions div
@@ -11,4 +19,8 @@ function loadDrawboard () {
     // TODO: remove h1 title telling whose turn it is and add to the row with the rest of game info  
     // add canvas to drawboard div
     drawboard.appendChild(canvas)
+}
+
+function beginDrawing () {
+
 }
