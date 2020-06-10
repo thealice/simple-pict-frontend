b253aec9af25795bd6b07667d59fec811f82f63f
Alice Freda <alicefreda@gmail.com>
2020-06-09 19:52:27 -0700

    add more event listeners to canvas for mousedown, up, and out

--- src/drawboard.js
+++ src/drawboard.js
@@ -1,6 +1,6 @@
 const canvas = document.createElement("canvas");
 const ctx = canvas.getContext('2d');
-canvas.width = "1242px";
+canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 canvas.style.border = "2px solid skyblue";
 ctx.strokeStyle = "black";
@@ -18,11 +18,14 @@ function loadDrawboard () {
     inst.style.display = "none";
     // TODO: remove h1 title telling whose turn it is and add to the row with the rest of game info  
     // add canvas to drawboard div
-    drawboard.appendChild(canvas)
+    drawboard.appendChild(canvas);
 }
 
 function beginDrawing(e) {
     console.log(e);
 }
 
-canvas.addEventListener('mousemove', draw) 
+canvas.addEventListener('mousemove', beginDrawing);
+canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
