b97384ea8d659f91451e5dc0d5c24c2c7141d66d
Alice Freda <alicefreda@gmail.com>
2020-06-12 22:02:37 -0700

    Game constructor takes in a theme object instead of theme_id now

--- src/js/game.js
+++ src/js/game.js
@@ -1,13 +1,13 @@
 
 class Game {
-    constructor(team1, team2, theme_id) {
+    constructor(team1, team2, theme) {
         this.team1 = new Team(team1);
         this.team2 = new Team(team2);
         this.teams = [this.team1, this.team2]
-        if (theme_id === 0) {
+        if (theme.id === 0) {
             this.theme = { name: "All themes", id: 0, prompts: Prompt.all }
         } else {
-            this.theme = Theme.all.find(themeObj => themeObj.id == theme_id);
+            this.theme = theme;
         }
         this.prompts = this.theme.prompts
         this.turn = this.team1;
